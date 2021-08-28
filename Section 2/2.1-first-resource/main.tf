9f7791459745183902a7801e74dae4da4e0e73a6
Jeyendran Sundaram <sjeyendran@gmail.com>
2021-08-28 14:17:18 +0530

    terraform s3 bucket creation

--- Section 2/2.1-first-resource/main.tf
+++ Section 2/2.1-first-resource/main.tf
@@ -1,4 +1,14 @@
+terraform {
+  required_providers {
+  aws = {
+    source = "hashicorp/aws"
+    version = "~> 3.0"
+     }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_s3_bucket" "very_first_s3_bucket" {
-  bucket = "packt-first-s3-bucket-terraform"
-  region = "us-west-2"
+  bucket = "jeyts-first-s3-bucket-terraform"
 }
